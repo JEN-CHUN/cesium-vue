361ccb1ca6fa266abc3adb69254a1c27c257298b
Jen CHUN <kayrenwork@gmail.com>
2023-11-18 22:33:59 +0800

    Adjust config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 
-import cesium from 'vite-plugin-cesium';
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
