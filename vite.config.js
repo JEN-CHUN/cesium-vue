784b0ec31f1b40ebb25674a7432148cea35a015e
Jen CHUN <kayrenwork@gmail.com>
2023-11-18 23:26:48 +0800

    Adjust some code of cesium

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-
+import cesium from 'vite-plugin-cesium'; // 引入插件
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cesium-vue/',
-  plugins: [vue()],
+  plugins: [vue(),cesium()],
   build: {
     assetsDir: 'assets'
   },
